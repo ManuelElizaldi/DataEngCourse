712c782f550d589cc9ffdbf776aa1646416086af
Manuel Elizaldi <manuelelizaldib@gmail.com>
2023-11-27 16:23:32 -0600

    Finished project

--- Week_2_PracticeProject/functions.py
+++ Week_2_PracticeProject/functions.py
@@ -30,9 +30,39 @@ def extract(url):
             break
     return df
 
+# Cleaning data function
 def transform(df):
     # Data cleaning
     df['GDP_USD_billion'] = df['GDP_USD_billion'].replace('—',np.nan, regex=True)
     df['GDP_USD_billion'] = df['GDP_USD_billion'].replace(',','', regex=True).astype(float)
+    df['GDP_USD_billion'] = round(df['GDP_USD_billion']/1000,2)
     df = df.sort_values(by='GDP_USD_billion', ascending=False)
     return df
+
+# Saving as CSV
+def load_to_csv(df, file_name, csv_path):
+    file_name = 'Countries_by_GDP.csv'
+    csv_path = fr'C:\Users\Manuel Elizaldi\Desktop\Learning-Testing\DataEngCourse\Week_2_PracticeProject\{file_name}'
+    df.to_csv(csv_path, index = False)
+
+# Saving data inside database
+def load_to_db(df, database, table_name):
+    conn = sqlite3.connect(database)
+    df.to_sql(table_name, conn, index = False, if_exists='replace')
+    conn.close()
+    
+# Function to query the table
+def run_query(query_statement, database):
+    conn = sqlite3.connect(database)
+    cursor = conn.cursor()
+    cursor.execute(query_statement)
+    rows = cursor.fetchall()
+    return rows
+
+# This function keeps a log of activities/tasks done
+def log_progress(message):
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
+    now = datetime.now() # get current timestamp 
+    timestamp = now.strftime(timestamp_format) 
+    with open("./etl_project_log.txt","a") as f: 
+        f.write(timestamp + ' : ' + message + '\n')
---
+++ Week_2_PracticeProject/project.py
@@ -0,0 +1,33 @@
+#### Fix pending
+
+
+from functions import *
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
+import sqlite3
+from datetime import datetime 
+
+url = 'https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
+table_attribs = ["Country", "GDP_USD_millions"]
+db_name = 'World_Economies.db'
+table_name = 'Countries_by_GDP'
+csv_path = './Countries_by_GDP.csv'
+
+
+log_progress('Preliminaries complete. Initiating ETL process')
+df = extract(url)
+log_progress('Data extraction complete. Initiating Transformation process')
+df = transform(df)
+log_progress('Data transformation complete. Initiating loading process')
+load_to_csv(df, 'World_Economies.csv', csv_path)
+log_progress('Data saved to CSV file')
+sql_connection = sqlite3.connect('World_Economies.db')
+log_progress('SQL Connection initiated.')
+load_to_db(df, db_name, table_name)
+log_progress('Data loaded to Database as table. Running the query')
+query_statement = f"SELECT * from {table_name} WHERE GDP_USD_billions >= 100"
+run_query(query_statement, sql_connection)
+log_progress('Process Complete.')
+sql_connection.close()
