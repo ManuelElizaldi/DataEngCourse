c9d51f31a07d82933c60e141c3edcb99a5e2bba2
Manuel Elizaldi <manuelelizaldib@gmail.com>
2023-11-30 10:44:26 -0600

    started writing functions

--- Week_2_PracticeProject/Graded_Project/functions.py
+++ Week_2_PracticeProject/Graded_Project/functions.py
@@ -12,3 +12,30 @@ def log_progress(message):
     timestamp = now.strftime(timestamp_format)  
     with open("./code_log.txt","a") as f: 
         f.write(timestamp + ' : ' + message + '\n')
+        
+        
+# This function will use requests to get html, then using beautiful soup we parse the html, from that we extract the data that we want:
+def extract(url):
+    r = requests.get(url).text
+    soup = BeautifulSoup(r, 'html.parser')
+    table = soup.find_all('tbody')
+    table = table[0]
+    rows = table.find_all('tr')
+    count = 0
+    
+    df = pd.DataFrame(columns = ['Name','MC_USD_Billion'])
+    data_dict = dict()
+
+    for row in rows:
+        if count < 11:
+            col = row.find_all('td')
+            if len(col) != 0:
+                data_dict = {'Name':col[1].contents[2].contents[0], 'MC_USD_Billion':col[2].contents[0].replace('\n','')}
+                df1 = pd.DataFrame(data_dict, index = [0])
+                df = pd.concat([df, df1])
+        else:
+            break
+
+    df['MC_USD_Billion'] = df['MC_USD_Billion'].astype(float)
+    
+    return df
