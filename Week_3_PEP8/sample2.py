fef362c5c17e003bcbca37c4c057645174607de3
Manuel Elizaldi <manuelelizaldib@gmail.com>
2023-12-01 16:51:33 -0600

    week 3 checkpoint

---
+++ Week_3_PEP8/sample2.py
@@ -0,0 +1,12 @@
+
+"""
+This is a docstring, it provides documentation regarding the file. It goes at the top of the script.
+You usually write waht the script is about. 
+"""
+def add(number1, number2):
+    return number1 + number2
+
+NUM1 = 4
+NUM2 = 5
+TOTAL = add(NUM1, NUM2)
+print("The sum of {} and {} is {}".format(NUM1, NUM2, TOTAL))
