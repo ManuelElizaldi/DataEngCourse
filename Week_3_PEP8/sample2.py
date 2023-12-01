4cc67c4b9c34585e792218a4283a90b6c8dba3b8
Manuel Elizaldi <manuelelizaldib@gmail.com>
2023-12-01 16:59:52 -0600

    Sample2.py is not pylint score -> 10/10

--- Week_3_PEP8/sample2.py
+++ Week_3_PEP8/sample2.py
@@ -1,10 +1,16 @@
 
 """
 This is a docstring, it provides documentation regarding the file. It goes at the top of the script.
-You usually write waht the script is about. For example this script is intended to teach me how to use
-Pylint. 
+You usually write what the script is about. For example,
+this script is intended to teach me how to use Pylint.
 """
+# Its best if the lines in the docstring are no longer than 100 characters
+
 def add(number1, number2):
+    """
+    This function is just a test. It will add 2 numbers.
+    """
+    # Function docstrings go inside the function
     return number1 + number2
 
 NUM1 = 4
