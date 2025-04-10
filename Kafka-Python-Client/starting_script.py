c16b978b4cc380092ea704d9fbf944ebf31827e1
Manuel Elizaldi <manuelelizaldib@gmail.com>
2025-04-09 20:40:36 -0500

    checkpoint

--- Kafka-Python-Client/starting_script.py
+++ Kafka-Python-Client/starting_script.py
@@ -1,4 +1,5 @@
 from kafka.admin import * 
+from kafka.admin import KafkaProducer
 import json
 
 # define the kafkaadminclient object
@@ -23,4 +24,20 @@ configs = admin_client.describe_configs(config_resources = [ConfigResource(Confi
 # most of the messages are in json format
 
 # defining a producer
+# Since Kafka produces and consumes messages in raw bytes, you need to encode our JSON messages and serialize them into bytes. For the value_serializer argument, you will define a lambda function to take a Python dict/list object and serialize it into bytes.
 producer = KafkaProducer(value_serializer = lambda v: json.dumps(v).encode("utf-8"))
+
+# now with the producer defined you can start sending messages:
+# the first argument defines the topic used, the second argument defines the key value pair message which will be serialized using a lambda function
+producer.send("bankbranch", {'atmid':1, 'transid':100})
+producer.send("bankbranch", {'atmid':2, 'transid':101})
+
+
+# kafka consumer
+# defining a kafka consumer and subscribing it to the topic bankbranch
+consumer = KafkaConsumer("bankbranch")
+
+# you can iterate and print the messages:
+for msg in consumer:
+    # we do decode because we need to encode the messages into bytes when sending data to kafka
+    print(msg.value.decode("utf-8"))
